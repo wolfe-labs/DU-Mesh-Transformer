e1734ee2b1e3f623bea9c351af535af6447e292f
Matheus Pratta <matpratta@gmail.com>
2024-04-27 17:33:16 -0300

    Added extra documentation on command arguments

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -7,7 +7,22 @@ import { EventType } from '../lib/types';
 app(async function main(modelPath, customGameDirectory) {
   const isDebugEnabled = !!JSON.parse(process.env.debug || 'false');
 
+  // Intro
+  const introText = `glTF Mesh Converter for Dual Universe by Wolfe Labs`;
+  console.log(`+-${'-'.repeat(introText.length)}-+`)
+  console.log(`| ${introText} |`)
+  console.log(`+-${'-'.repeat(introText.length)}-+`)
+  console.log(``)
+
+  // Handles cases where no model is passed
+  if (!modelPath || modelPath.length == 0) {
+    console.info(`Missing model path as first parameter, make sure you supply a valid glTF file!`);
+    console.info(`Optionally, you can also supply the game directory if you have a custom installation.`);
+    return;
+  }
+
   // Loads our mesh
+  console.log(`Loading file for processing: ${modelPath}`);
   const meshTransformer = await DuMeshTransformer.fromFile(modelPath);
 
   // Optionally set our game directory
@@ -20,7 +35,7 @@ app(async function main(modelPath, customGameDirectory) {
 
   // Only show debug stuff with debug env variable
   if (isDebugEnabled) {
-    meshTransformer.events().on(EventType.TRANSFORM_START, () => console.log('[STATUS] Model processing started!\n'));
+    meshTransformer.events().on(EventType.TRANSFORM_START, () => console.log('\n[STATUS] Model processing started!\n'));
     meshTransformer.events().on(EventType.TRANSFORM_FINISH, () => console.log('[STATUS] Model processing finished!\n'));
     meshTransformer.events().on(EventType.TRANSFORM_NEXT, () => console.log(''));
     meshTransformer.events().on(EventType.DEBUG, message => console.log('[DEBUG]', message));
@@ -30,13 +45,18 @@ app(async function main(modelPath, customGameDirectory) {
   const dir = path.dirname(modelPath);
   const basename = path.basename(modelPath, path.extname(modelPath));
 
-  // Processes our mesh as desired
+  // Processes our mesh as desired and saves it
+  const outputFile = path.join(dir, `${basename}.out`);
+  if (!isDebugEnabled) {
+    console.log('Mesh processing started!');
+  }
   await meshTransformer
     .withBaseColors()
     .withTextures()
     .withUvMaps()
     .withHdrEmissive()
     .withSeparatedElements()
-    .saveToFile(path.join(dir, `${basename}.out`), false);
-  console.log('Mesh saved successfully!');
+    .saveToFile(outputFile, false);
+  console.log(`Mesh saved successfully!`);
+  console.log(`Save location: ${outputFile}`);
 });
